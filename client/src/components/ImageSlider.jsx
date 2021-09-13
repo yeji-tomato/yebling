c86b9354d73e90fa8b406a590de40f16c165c293
Lee-Ye-Ji <dpwl30559@gmail.com>
2021-09-14 03:28:30 +0900

    image path

--- client/src/components/ImageSlider.jsx
+++ client/src/components/ImageSlider.jsx
@@ -3,13 +3,14 @@ import { Carousel } from 'antd'
 
 function ImageSlider(props) {
 
+    let src = process.env.NODE_ENV === 'production' ? `https://yebling.herokuapp.com/` : `http://localhost:5000/`
     return (
         <div>
             <Carousel autoplay>
                     {props.images.map((image, index) => (
                         <div key={index}>
                             <img style={{width: '100%', maxHeight:'300px'}} 
-                            src={`http://localhost:5000/${image}`} alt={`${index}`} />
+                            src={`${src}${image}`} alt={`${index}`} />
                         </div>
                     ))}
             </Carousel>
