c3a9fb9205a9b4f8384c20566047847b4b8a595e
Lee-Ye-Ji <dpwl30559@gmail.com>
2021-09-25 23:53:08 +0900

    auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -10,11 +10,13 @@ let auth = (req, res, next) => {
     // 토큰을 복호화한 후 유저를 찾는다.
     User.findByToken(token, (err, user) => {
         if(err) throw err
-        if(!user) return res.json({ isAuth: false, error: true})
-
-        req.token = token
-        req.user = user
-        next()
+        if(!user){
+            return res.json({ isAuth: false, error: true})
+        }else{
+            req.token = token
+            req.user = user
+            next()
+        }
     })
 }
 
