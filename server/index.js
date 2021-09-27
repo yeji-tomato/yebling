564aad1889076b054e5c527c98c06fdaff63e45e
Lee-Ye-Ji <dpwl30559@gmail.com>
2021-09-28 01:37:54 +0900

    404 fixed

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser')
 const config = require('./config/key')
-// const cors = require('cors');
+const cors = require('cors');
 
 
 // let corsOptions =
@@ -28,7 +28,10 @@ app.use(express.urlencoded({ extended: true }))
 // application/json
 app.use(express.json())
 app.use(cookieParser())
-// app.use(cors(corsOptions));
+app.use(cors({
+        origin: 'http://localhost:3000',
+        credentials: true,    
+    }));
 
 const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI,{
@@ -37,12 +40,13 @@ mongoose.connect(config.mongoURI,{
 .catch(err => console.log(err))
 
 // 리액트 정적 파일 제공
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// 라우트 설정
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // 라우트 설정
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    });
+}
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/product', require('./routes/product'));
