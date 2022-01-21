9c4d5da651170a61ccc7ac51a5319841fb6d5797
Lee-Ye-Ji <dpwl30559@gmail.com>
2022-01-21 13:47:16 +0900

    modify: [error] cors

--- client/src/_actions/config.js
+++ client/src/_actions/config.js
@@ -2,5 +2,5 @@
 
 // let urlTarget = process.env.NODE_ENV === 'production' ?  "https://yebling.herokuapp.com" : 'http://localhost:5000';
 
-export const USER_SERVER = 'https://yebling.herokuapp.com/api/users';
-export const PRODUCT_SERVER = 'https://yebling.herokuapp.com/api/product';
+export const USER_SERVER = '/api/users';
+export const PRODUCT_SERVER = '/api/product';
